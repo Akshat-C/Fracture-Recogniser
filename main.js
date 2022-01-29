90d9cf2caabfdd36c82bfae0bbaedb8c4b4cee93
Akshat-C <85225437+Akshat-C@users.noreply.github.com>
2022-01-29 16:19:46 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ function capture_img()
 
 console.log("ml5 version is:", ml5.version);
 
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Gkk3cF6_q/model.json", modelLoaded);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/FaWUtr-iv/model.json", modelLoaded);
 function modelLoaded()
 {
     console.log("Model is loaded");
@@ -44,17 +44,34 @@ function gotResults(error, results)
     if (error)
     {
         console.error(error);
-    } else
+    } else 
     {
         console.log(results);
-        document.getElementById("member_name").innerHTML = results[0].label;
-        accur = results[0].confidence*100;
-        document.getElementById("accuracy").innerHTML = accur.toFixed(2) + "% ";
-    }
-
-    if (results[0].label == "Fracture")
+        fracture_name = results[0].label;
+        fracture_accuracy = results[0].confidence;
+        document.getElementById("fracture_name").innerHTML = fracture_name;
+        document.getElementById("accuracy").innerHTML = fracture_accuracy;
+        
+        if (fracture_name == "Spiral Fracture")
     {
-        document.getElementById("fracture_info").innerHTML = "A fracture occurs when a bone is broken due to an accident or fall. Fractures can happen anywhere there are bones but mostly happen in the bones of the arms or legs. They can take a few weeks or months to heal depending on their severity.";
-        i++;
+        document.getElementById("fracture_info").innerHTML = "Spiral fractures are fractures that spiral around bone and usually occur in long bones of the body.";
+        document.getElementById("cartoon_frac").src = "Spiral.jpg";
+    } else if (fracture_name == "Segemental Fracture")
+    {
+        document.getElementById("fracture_info").innerHTML = "Segmental fractures are fractures that leave behind a floating section of bone because the bone is fractured in 2 or more areas. They mainly occur in the longs bones of the body.";
+        document.getElementById("cartoon_frac").src = "Segmental.png";
+    } else if (fracture_name == "Comminuted Fracture")
+    {
+        document.getElementById("fracture_info").innerHTML = "Comminuted fractures is a type of fracture that has resulted in 3 or more breaks on the bone and has fragments of the bone at the fracture site.";
+        document.getElementById("cartoon_frac").src = "Comminuted.png";
+    } else if (fracture_name == "Greenstick Fracture")
+    {
+        document.getElementById("fracture_info").innerHTML = "Greenstick fractures are fractures occur when the bone is broken and bent but doesn't seperate into pieces. This fracture mainly occurs in children.";
+        document.getElementById("cartoon_frac").src = "Greenstick.jpg";
+    } else if (fracture_name == "Transverse Fracture")
+    {
+        document.getElementById("fracture_info").innerHTML = "Transverse Fractures are fractures that cause breaks which are a straight line through the bone."
+        document.getElementById("cartoon_frac").src = "Transverse.png";
+    } 
     }
 }
